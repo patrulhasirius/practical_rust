e36d7007ba4850b277319150c8f03dbdc1c01ccf
Lucas Ribeiro <estg-lnr@tce.rs.gov.br>
2024-04-17 16:50:00 -0300

    tests, anyhow, clap, colored and stdin

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -24,15 +24,58 @@ Hello I'm a cat
 
 */
 
-fn main() {
-    let message = std::env::args()
-        .nth(1)
-        .expect("Missing the message. Usage: catsay <message>");
+use std::io::{self, Read};
 
-    println!("{}", message);
-    println!(" \\");
-    println!("  \\");
-    println!("      /\\_/\\");
-    println!("     ( o o )");
-    println!("     =( I )=");
+use anyhow::{Context, Result};
+use clap::Parser;
+use colored::Colorize;
+
+#[derive(Parser)]
+struct Options {
+    #[clap(default_value = "Meow!")]
+    /// What does the cat say?
+    message: String,
+    #[clap(short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
+    #[clap(short = 'f', long = "file")]
+    /// Load the cat picture from the specified file
+    catfile: Option<std::path::PathBuf>,
+    #[clap(short = 'i', long = "stdin")]
+    /// Read the message from STDIN instead of the argument
+    stdin: bool,
+}
+
+fn main() -> Result<()> {
+    let options = Options::parse();
+    let mut message = String::new();
+    if options.stdin {
+        io::stdin().read_to_string(&mut message)?;
+        message = message.trim_end().to_string();
+    } else {
+        message = options.message;
+    }
+
+    let eye = if options.dead { "x" } else { "o" };
+
+    match &options.catfile {
+        Some(path) => {
+            let cat_template = std::fs::read_to_string(path)
+                .with_context(|| format!("Could not read file {:?}", path))?;
+            let eye = format!("{}", eye.red().bold());
+            let cat_picture = cat_template.replace("{eye}", &eye);
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!("{}", cat_picture);
+        }
+        None => {
+            println!("{}", message.bright_yellow().underline().on_purple());
+            println!(" \\");
+            println!("  \\");
+            println!("      /\\_/\\");
+            println!("     ( {eye} {eye} )", eye = eye.red().bold());
+            println!("     =( I )=");
+        }
+    }
+
+    Ok(())
 }
