0b84829b89d4ab459a6880aee903fe324b5f1f11
Lucas Ribeiro <estg-lnr@tce.rs.gov.br>
2024-04-29 18:07:34 -0300

    catsay-gui fix

--- catsay-gui/src/main.rs
+++ catsay-gui/src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Box as GtkBox, Image, Label, Orientation};
 
 fn main() {
     let app = Application::new(Some("com.shinglyu.catsay-gui"), Default::default());
@@ -7,6 +7,18 @@ fn main() {
         let window = ApplicationWindow::new(app);
         window.set_title("Catsay");
         window.set_default_size(350, 70);
+        let layout_box = GtkBox::new(Orientation::Vertical, 0);
+
+        let label = Label::new(Some("Meow!\n     \\\n       \\"));
+
+        layout_box.add(&label);
+
+        let cat_image = Image::from_file("./images/cat.png");
+
+        layout_box.add(&cat_image);
+
+        window.add(&layout_box);
+
         window.show_all();
     });
     app.run();
