edf969873197d67be4c1e1bea1fc8b3b8813ee8d
Lucas Ribeiro <estg-lnr@tce.rs.gov.br>
2024-04-30 13:54:18 -0300

    upgrade

--- catsay-gui-glade/src/main.rs
+++ catsay-gui-glade/src/main.rs
@@ -8,7 +8,35 @@ fn build_ui(app: &gtk::Application) {
 
     window.set_application(Some(app));
 
-    window.show_all()
+    // Inputs
+    let message_input: gtk::Entry = builder.object("message_input").unwrap();
+
+    // Submit button
+    let button: gtk::Button = builder.object("generate_btn").unwrap();
+
+    // Outputs
+    let is_dead_switch: gtk::Switch = builder.object("is_dead_switch").unwrap();
+
+    let message_output: gtk::Label = builder.object("message_output").unwrap();
+
+    let image_output: gtk::Image = builder.object("image_output").unwrap();
+    let image_output_clone = image_output.clone(); // low-cost clone
+
+    button.connect_clicked(move |_| {
+        message_output.set_text(&format!("{}\n \\\n \\", message_input.text().as_str()));
+        image_output_clone.show();
+        let is_dead = is_dead_switch.is_active();
+
+        if is_dead {
+            image_output_clone.set_from_file(Some("./images/cat_dead.png"))
+        } else {
+            image_output_clone.set_from_file(Some("./images/cat.png"))
+        }
+        image_output_clone.show();
+    });
+
+    window.show_all();
+    image_output.hide();
 }
 
 fn main() {
