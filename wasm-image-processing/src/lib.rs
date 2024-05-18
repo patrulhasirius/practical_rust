1c68d38e15426da815e7d1cb6b59f6f79a92a3e7
Lucas Ribeiro <patruop@gmail.com>
2024-05-18 00:00:17 -0300

    finished wasm-image-proceessing

--- wasm-image-processing/src/lib.rs
+++ wasm-image-processing/src/lib.rs
@@ -1,5 +1,8 @@
 mod utils;
 
+use image::imageops;
+use image::RgbaImage;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,6 +11,10 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-image-processing!");
+pub fn shrink_by_half(original_image: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+    let image: RgbaImage = image::ImageBuffer::from_vec(width, height, original_image).unwrap();
+    let output_image =
+        imageops::resize(&image, width / 2, height / 2, imageops::FilterType::Nearest);
+
+    output_image.into_vec()
 }
